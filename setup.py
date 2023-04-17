b26c8109090efcf1c639401806582313fa33554f
James Stevenson <28961606+user1342@users.noreply.github.com>
2023-04-17 21:21:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='PocketSmali',
-      version='0.2',
+      version='0.3',
       description='A modular and extendable Python tool for emulating simple SMALI methods.',
       author='James Stevenson',
       author_email="hi@jamesstevenson.me",
       url='https://github.com/user1342/PocketSmali',
-      packages=['PocketSmali'],
+      packages=['PocketSmali','PocketSmali.opcode_handlers'],
      )
